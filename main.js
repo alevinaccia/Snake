246b767bfa86bc25ff455812eed6891c25bb6caf
Alessandro Vinaccia <vinacciaalessandro@gmail.com>
2019-02-01 23:55:28 +0100

    Started the main.js File

--- main.js
+++ main.js
@@ -17,12 +17,40 @@ function preload() {
     game.load.image('snake', 'Assets/corpo.png');
     game.load.image('food', 'Assets/cibo.png');
 
+    
 }
 
 function create() {
-    
+
+    player = game.add.image(W/2, H/2, 'snake');
+
+    setAnchor(player,0.5,0.5)
+
+    cursors = game.input.keyboard.createCursorKeys();
+
 }
 
 function update(){
-    
+    checkMovement();
+}
+
+function checkMovement(){
+    if(cursors.up.isDown){
+
+    }
+    else if(cursors.down.isDown){
+
+    }
+    else if(cursors.left.isDown){
+
+    }
+    else if(cursors.right.isDown){
+
+    }
+
+}
+
+function setAnchor(sprite,xValue,yValue){
+    sprite.anchor.x = xValue;
+    sprite.anchor.y = yValue;
 }
